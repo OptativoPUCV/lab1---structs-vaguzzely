4734cf018efb9911dedb77a26cac4be71f8ab6b7
Replit user <>
2023-03-14 18:16:42 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -13,7 +13,9 @@ Programe la función void swap(int *a, int *b), la cual
 intercambia los valores de las variables apuntadas por a y b.
 */
 void swap(int *a, int *b) {
-
+  aux = *a;
+  *a = *b;
+  *b = aux;
 }
 
 /*
