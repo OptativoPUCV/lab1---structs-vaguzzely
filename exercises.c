d3cfcf9edc9bc65dbfbb241c1bfbc11c88057bba
Replit user <>
2023-03-18 19:24:29 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -117,10 +117,10 @@ actualiza el vector `c` con el resultado de la suma.
 void sumaV(Vector * a, Vector * b, Vector * c) 
 {
   int i = 0;
-  while (i < a->capacidad) {
+  do {
     c->datos[i] = a->datos[i] + b->datos[i];
-    i+1;
-  }
+    i = i+1;
+  } while (i < a->capacidad);
 }
 
 /*
